6ec2141d5c97d4fb5b26b45d3f6bc6617e2d456d
adud <antonin.dudermel@caramail.fr>
2022-01-10 02:21:30 +0100

    add a simple train function
    with possibility to train the discriminator more than the generator

--- config.py
+++ config.py
@@ -13,7 +13,13 @@ SAMPLE_RATE = 22050
 
 # Model parameters
 
+LATENT_DIM = 100  # dimensions of the latent space (also Generator input size)
 
+# Adam optimizer parameters (see p6)
+
+LR = 1e-3  # Learning rate
+B1 = 0.5   # beta1
+B2 = 0.9   # beta2
 
 
 # Training parameters
@@ -22,3 +28,6 @@ BATCH_SIZE = 16
 FADE_IN = 64  # in samples
 FADE_OUT = 64  # in samples
 TRAIN_PATH = './dataset/'
+
+EX_GEN = 10  # number of examples to generate for log
+EX_PATH = 'examples'
--- train.py
+++ train.py
@@ -1,18 +1,83 @@
 import torch
 import torchaudio
 from torch.utils.data import DataLoader
-from config import *
+from torch import optim
+import config as c
 import utils.dataset
 import utils.transform
+import os
 
 transforms = torch.nn.Sequential(
-                        torchaudio.transforms.Fade(FADE_IN, FADE_OUT),
+                        torchaudio.transforms.Fade(c.FADE_IN, c.FADE_OUT),
                         utils.transform.TransposeTransform(),
-                        utils.transform.ZeroPadTransform(FFT_SIZE),
-                        utils.transform.MdctTransform(FFT_SIZE),
+                        utils.transform.ZeroPadTransform(c.FFT_SIZE),
+                        utils.transform.MdctTransform(c.FFT_SIZE),
                         utils.transform.ToTensorTransform()
                         )
 
-training_data = utils.dataset.MP3NetDataset(TRAIN_PATH, transform=transforms)
+training_data = utils.dataset.MP3NetDataset(c.TRAIN_PATH, transform=transforms)
 
-train_dataloader = DataLoader(training_data, BATCH_SIZE, shuffle=True)
+train_dataloader = DataLoader(training_data, c.BATCH_SIZE, shuffle=True)
+
+
+def train(arch, loss, epoch, dis_bonus, flog, device):
+    """trains the generator and the discriminator
+    arch: a pair (generator, discriminator) of a GAN architecture
+    loss: a loss function
+    optim: an optimizer class
+    epoch: the number of training iterations on the dataset
+    dis_bonus: the number of added iterations on the discriminator
+    flog: frequency of logs (number of batchs before printing)
+          a negative value means no log at all
+    device: device for computations (should be the same as arch)"""
+    gen, dis = arch
+    dis_opt = optim.Adam(dis.parameters(), lr=c.LR, betas=(c.B1, c.B2))
+    gen_opt = optim.Adam(gen.parameters(), lr=c.LR, betas=(c.B1, c.B2))
+    real_lab = torch.ones(c.BATCH_SIZE, 1, device=device)
+    fake_lab = torch.zeros(c.BATCH_SIZE, 1, device=device)
+
+    if flog >= 0:
+        fix_lat = torch.randn(c.EX_GEN, c.LATENT_DIM, device=device)
+        if os.path.exists(c.EX_PATH):
+            raise FileExistsError("training would erase logs: aborting")
+        os.makedirs(c.EX_PATH)
+
+    for ep in range(epoch):
+        if flog >= 0:
+            print(f"## EPOCH {epoch} ##")
+            os.makedirs(os.join(c.EX_PATH, str(ep)))
+        for i, data in enumerate(train_dataloader):
+            # train the discriminator
+            for _ in range(dis_bonus):
+                lat_vect = torch.randn(c.BATCH_SIZE, c.LATENT_DIM,
+                                       device=device)
+                with torch.no_grad():
+                    fake = gen(lat_vect)
+
+                real = data.to(device)
+
+                dfake_loss = loss(dis(fake), fake_lab)
+                dreal_loss = loss(dis(real), real_lab)
+                dis_loss = (dfake_loss + dreal_loss) / 2
+
+                dis.zero_grad()
+                dis_loss.backward()
+                dis_opt.step()
+
+            # train the generator
+            lat_vect = torch.randn(c.BATCH_SIZE, c.LATENT_DIM, device=device)
+            fake = gen(lat_vect)
+            with torch.no_grad():
+                guess = dis(fake)
+
+            gen_loss = loss(guess, real_lab)
+            gen.zero_grad()
+            gen_loss.backward
+            gen_opt.step()
+
+            if not i % flog:
+                print(f"\tbatch {i}/{c.BATCH_SIZE}: {gen_loss}, {dis_loss}")
+                with torch.no_grad():
+                    out = gen(fix_lat)
+                    with open(os.join(c.EX_PATH, str(ep), str(i))) as f:
+                        torch.save(out, f)
